0a2bd65f0148f58c2f1cae0f96a9bcbd0f258e82
PalladaV <aybol1b5c@yahoo.com>
2022-05-05 18:25:24 +0300

    HW4

---
+++ HW4/webtests/src/test/java/com/geekbrains/TriangleTest.java
@@ -0,0 +1,43 @@
+package com.geekbrains;
+
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.security.pkcs11.wrapper.Functions;
+
+import static com.geekbrains.HW4.TriangleArea.calculateArea;
+
+public class TriangleTest {
+    private static Logger logger = LoggerFactory.getLogger("TriangleTest");
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Метод выполнится 1 раз перед всеми тестами");
+        logger.info("info log");
+        logger.warn("warn log");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Метод выполнится перед каждым тестами");
+    }
+
+    @Test
+    @DisplayName("Позитивный тест площади треугольника")
+    void givenTriangleAreaWhencalculateAreaThenTrue() {
+        double result = calculateArea(3, 4, 4);
+        assertTrue(result);
+    }
+
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("Метод выполнится 1 раз после каждого теста");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Метод выполнится 1 раз после всех тестов");
+    }
+
+}
